01055ec4a8bef96aed628c188f44b30c4872bbf4
mchellmer <mchellmer@gmail.com>
2025-02-10 07:44:37 +0000

    midimodel: setup data configuration app which will seed data for midi devices to db

---
+++ NineteenSeventyTwo.EightBitSaxLounge.DataManagerApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// Load midi device config from json
+
+// Seed database with midi device loaded data
